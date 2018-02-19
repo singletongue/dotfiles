beb97578e44154ce8906f444906dbab79770a72c
Masatoshi Suzuki <sappukei.suzuki@gmail.com>
2018-02-19 11:36:18 +0900

    Added colorscheme file for blink shell

---
+++ colorscheme/base16-material.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#263238", "#f07178", "#c3e88d", "#ffcb6b", "#82aaff", "#c792ea", "#89ddff", "#eeffff", "#546e7a", "#f78c6c", "#2e3c43", "#314549", "#b2ccd6", "#eeffff", "#ff5370", "#ffffff"]);
+t.prefs_.set('foreground-color', "#eeffff");
+t.prefs_.set('background-color', "#263238");
+t.prefs_.set('cursor-color', 'rgba(238,255,255,0.5)');
